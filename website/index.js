c269b78152d9e65da4ebffdec25ee2d7c1094a0e
minipliy <lukas@minipliy.net>
2026-02-10 13:34:33 +0100

    Website UI/UX update

--- website/index.js
+++ website/index.js
@@ -3,7 +3,7 @@ import init, * as wasm from "./wasm.js"
 const WIDTH = 64
 const HEIGHT = 32
 const SCALE = 15
-const TICKS_PER_FRAME = 10
+const TICKS_PER_FRAME = 9
 let anim_frame = 0
 
 const canvas = document.getElementById("canvas")
@@ -33,6 +33,7 @@ Q and E to move.
 Keep the ball bouncing to destroy all tiles.`,
 
     "c8games/INVADERS": `Space Invaders:
+A to start
 Q and E to move
 W to shoot
 Destroy all enemies before time runs out.`,
